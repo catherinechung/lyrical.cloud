b87ed4aa33a94a9df096148e1289c9e55b5be4ca
Catherine Chung <cathercc@usc.edu>
2017-02-25 10:38:16 -0800

    links for song list page

--- frontend/js/songList.js
+++ frontend/js/songList.js
@@ -1,15 +1,14 @@
-
 $(document).ready(function() {
 
   // JSON object, parsing to get var word, also array of songs
   // parse through JSON object
 
-  // set the title of the Song List Page
-  var title = {word: "One Dance"}
+  /* (1) Set the title of the Song List page */
+  var title = {word: "Word"}
     // will parse in the word from the JSON object later (object.word)
   document.getElementById("songListTitle").innerHTML = title.word;
 
-  // populate song List
+  /* (2) Populate song list */
     // from JSON object --> object.song, object.occurences
   var songs = [
           name = ['One Dance','Bad and Boujee', 'Shape of You', 'Chained to the Rhythm', 'Closer', 'Starboy'],
@@ -17,33 +16,37 @@ $(document).ready(function() {
       ];
 
   function makeOL(array1, array2) {
-      // Create the list element:
+      // Create the list element
       var list = document.createElement('ol');
 
       for(var i = 0; i < array1.length; i++) {
-          // Create the list item:
+          // Create the list item
           var item = document.createElement('li');
 
-          // Set its contents:
-          item.appendChild(document.createTextNode(array1[i] +" " + "(" + array2[i] + ")"));
+          // create the anchor item
+          var a = document.createElement('a');
+          a.setAttribute('href', "https://www.google.com");
 
-          // Add it to the list:
+          // Set its contents
+          a.textContent = array1[i] +" " + "(" + array2[i] + ")";
+
+          // create link to lyrics
+          item.appendChild(a);
+
+          // Add it to the list
           list.appendChild(item);
       }
 
-      // Finally, return the constructed list:
+      // Return the constructed list
       return list;
   }
 
   // Add the contents of songs[0] to #songList:
   document.getElementById('songList').appendChild(makeOL(songs[0], songs[1]));
 
-  // create links for each song
-
-
 });
 
-  // back button, returns to Word Cloud Page
-  function returnWordCloud() {
-
-  }
+/* Back button, returns to Word Cloud Page */
+function returnWordCloud() {
+    window.location.href = "wordCloud.html"
+}
