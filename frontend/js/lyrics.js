e47e846a52ce97e153bb622edf30e817c6557a2b
Catherine Chung <cathercc@usc.edu>
2017-02-25 23:06:19 -0800

    some css stuff as well

--- frontend/js/lyrics.js
+++ frontend/js/lyrics.js
@@ -2,10 +2,12 @@
 var song = {lyrics:"Baby I like your style \r\n Grips on your legs \r\n Front way, back way \r\n You know that I don't play \r\n Streets not safe \r\n But I never run away \r\n Even when I'm away \r\n OT, OT is never much love when we go OT \r\n I pray to make it back in one piece \r\n I pray, I pray \r\n That's why I need a one dance \r\n Got the Hennessy in my hand \r\n One more time 'fore I go \r\n I have powers taking ahold on me \r\n I need a one dance \r\n Got the Hennessy in my hand \r\n One more time 'fore I go \r\n I have powers taking ahold on me"};
 
 
-document.onreadystatechange = function () {
+//document.onreadystatechange = function () {
+$(document).ready(function() {
     var songName = localStorage.getItem('songName');
     var artist = localStorage.getItem('artist');
     var word = " " + localStorage.getItem('word')+ " ";
+    console.log(word);
     var lyrics;
 
     $.ajax({
@@ -19,7 +21,7 @@ document.onreadystatechange = function () {
      //query musixmatch given song and artist to get lyrics
 
 
-    if (document.readyState == "complete") {
+    //if (document.readyState == "complete") {
      document.title = songName + " by " + artist;
      document.getElementById("title").innerHTML = songName + " by " + artist;
 
@@ -28,5 +30,5 @@ document.onreadystatechange = function () {
 	 var regex = new RegExp('('+word+')', 'ig');
      lyrics = lyrics.replace(regex, '<span class="highlight">$1</span>');
      document.getElementById("lyrics").innerHTML = lyrics;
-   }
+   //}
 }
