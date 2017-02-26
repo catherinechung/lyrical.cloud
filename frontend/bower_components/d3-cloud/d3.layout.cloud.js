6b9fbfff9fa81ff95a34ad91180d971ab284360b
Catherine Chung <cathercc@usc.edu>
2017-02-25 21:32:38 -0800

    added favicon

--- frontend/bower_components/d3-cloud/d3.layout.cloud.js
+++ frontend/bower_components/d3-cloud/d3.layout.cloud.js
@@ -338,9 +338,24 @@
   }
 
   function archimedeanSpiral(size) {
-    var e = size[0] / size[1];
+    // var e = size[0] / size[1];
+    // return function(t) {
+    //   return [e * (t *= .1) * Math.cos(t), t * Math.sin(t)];
+    // };
+    var dy = 4,
+        dx = dy * size[0] / size[1],
+        x = 0,
+        y = 0;
     return function(t) {
-      return [e * (t *= .1) * Math.cos(t), t * Math.sin(t)];
+      var sign = t < 0 ? -1 : 1;
+      // See triangular numbers: T_n = n * (n + 1) / 2.
+      switch ((Math.sqrt(1 + 4 * sign * t) - sign) & 3) {
+        case 0:  x += dx; break;
+        case 1:  y += dy; break;
+        case 2:  x -= dx; break;
+        default: y -= dy; break;
+      }
+      return [x, y];
     };
   }
 
