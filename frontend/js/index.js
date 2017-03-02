d627d734d37f1aab8e23327b1a54157cd86b4b9a
codeon19 <breck@usc.edu>
2017-03-01 23:16:27 -0800

    artist pass string

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -13,7 +13,6 @@ const YES_SEARCH = true;
 const MIN_LENGTH = 3;
 
 var searchedState = NO_SEARCH;
-var artistID;
 
 // initial states
 $("#searchButton").prop("disabled", true);
@@ -37,30 +36,7 @@ $("#searchButton").click(function() {
 
   $.ajax({
     type : 'GET',
-    url: 'http://localhost:8080/api/wordcloud/new/' + artistID,
-    dataType: 'jsonp',
-    success: function(data) {
-      tags = data;
-      update();
-    },
-    error: function(err) {
-      console.log(err);
-    }
-  });
-
-});
-
-$("#addButton").click(function() {
-  $("#vis").show();
-
-  var $currentArtists = document.getElementById("artist").innerText;
-  var $artistName = $("#automplete-1").val();
-  $artistName = $artistName[0].toUpperCase() + $artistName.slice(1);
-  document.getElementById("artist").innerHTML = $currentArtists + ", " + $artistName;
-
-  $.ajax({
-    type : 'GET',
-    url: 'http://localhost:8080/api/wordcloud/merge/' + $artistName,
+    url: 'http://localhost:8080/api/wordcloud/new/' + $artistName,
     dataType: 'jsonp',
     success: function(data) {
       tags = data;
@@ -120,7 +96,6 @@ $("#automplete-1").autocomplete({
     select: function(event, ui) {
       event.preventDefault();
       $("#automplete-1").val(ui.item.artist);
-      artistID = ui.item.id);
 
       $("#searchButton").prop("disabled", false);
       $("#searchButton").removeClass("btn-class-disabled");
